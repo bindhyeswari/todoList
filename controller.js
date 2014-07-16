4b84b436a4fec732859d9a36138c646719b6d038
superuser <superuser@Sudesnas-MacBook-Pro-2.local>
2014-07-15 21:54:11 -0700

    start

---
+++ controller.js
@@ -0,0 +1,29 @@
+
+
+var todoApp = angular.module('todoApp', []);
+
+todoApp.controller('todoController', function ($scope, $http) {
+
+    $scope.tasks = [];
+    $scope.inpTask = '';
+
+    $scope.createTask = function (event) {
+        if (event.keyCode === 13) {
+            $scope.tasks.push({
+                name: $scope.inpTask,
+                done: false
+            });
+            $scope.inpTask = '';
+        }
+
+    };
+
+    $scope.deleteTask = function (task) {
+        $scope.tasks.splice($scope.tasks.indexOf(task), 1);
+    };
+
+    $scope.doneTask = function (task) {
+        $scope.tasks[$scope.tasks.indexOf(task)].done = true;
+    };
+
+});
